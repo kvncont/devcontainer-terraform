a952ff99d521f9e2a183b7f94f2781e89a654e34
Kevin Contreras <kvncont@gmail.com>
2023-01-18 06:12:58 +0000

    feat: add bash script

---
+++ .devcontainer/scripts/show-tools-versions.sh
@@ -0,0 +1,20 @@
+#!\bin\bash
+
+echo -e "<<<<<<<<<< Git >>>>>>>>>>"
+git version
+echo -e "\n<<<<<<<<<< GitHub >>>>>>>>>>"
+gh version
+echo -e "\n<<<<<<<<<< Azure CLI >>>>>>>>>>"
+az --version
+echo -e "\n<<<<<<<<<< Terraform >>>>>>>>>>"
+terraform version
+echo -e "\n<<<<<<<<<< Terraform Docs >>>>>>>>>>"
+terraform-docs version
+echo -e "\n<<<<<<<<<< TF Sec >>>>>>>>>>"
+tfsec --version
+echo -e "\n<<<<<<<<<< Kubectl >>>>>>>>>>"
+kubectl version
+echo -e "\n<<<<<<<<<< Helm >>>>>>>>>>"
+helm version
+echo -e "\n<<<<<<<<<< Minikube >>>>>>>>>>"
+minikube version
